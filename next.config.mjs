009089113c7e26dfca2fdf4fd9452793761e08af
iabdu8 <62561688+iabdu8@users.noreply.github.com>
2026-03-04 23:10:19 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+output: 'export',  // هذا السطر هو السر، يجعل الموقع يشتغل كملفات ثابتة
+images: {
+unoptimized: true, // ضروري لأن Cloudflare Pages لا يدعم تحسين الصور التلقائي في وضع الـ export
+},
 typescript: {
 ignoreBuildErrors: true,
 },
